4b70829b129d84049bd253f14b19a201b7d897db
SinCatGit <sincat@126.com>
2016-08-03 14:51:50 +0800

    添加topic_list按照时间倒序显示

--- wechat/views.py
+++ wechat/views.py
@@ -113,7 +113,7 @@ def topic_list(request):
     context = {}
     # 文章信息
     params = request.GET.copy()
-    _obj_list = Topic.objects.order_by('-id').values('id', 'wechat__id', 'wechat__name', 'avatar', 'title', 'url', 'publish_time', 'available')
+    _obj_list = Topic.objects.order_by('-publish_time').values('id', 'wechat__id', 'wechat__name', 'avatar', 'title', 'url', 'publish_time', 'available')
 
     paginator = Paginator(_obj_list, 50 )  # Show 10 contacts per page
 
